2549a67c8f21dee3dde69343fb7a92dec84c1a65
Andrei Paplauski <poplavski.andrei@gmail.com>
2016-10-07 15:28:52 +0200

    Add files via upload

--- instrlist.h
+++ instrlist.h
@@ -1,4 +1,30 @@
 #ifndef ILIST.H
 #define ILIST.H
 
+
+typedef struct listItem
+{
+  tInstr Instruction;
+  struct listItem *nextItem;
+} tListItem;
+    
+
+typedef struct
+{
+  struct listItem *first;  // ukazatel na prvni prvek
+  struct listItem *last;   // ukazatel na posledni prvek
+  struct listItem *active; // ukazatel na aktivni prvek
+} tListOfInstr;
+
+
+void listInit(tListOfInstr *L);
+void listFree(tListOfInstr *L);
+void listInsertLast(tListOfInstr *L, tInstr I);
+void listFirst(tListOfInstr *L);
+void listNext(tListOfInstr *L);
+tInstr *listGetData(tListOfInstr *L);
+void listGoto(tListOfInstr *L, void *gotoInstr); 
+void *listGetPointerLast(tListOfInstr *L);
+
+
 #endif
