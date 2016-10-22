8f96a9eaf98ca12b19180ea901b5e106476dddcf
davidV23 <vlcedav@seznam.cz>
2016-10-22 21:19:19 +0200

    main_int_string_double_deklarace
    pridane parsery pro deklarace stringu a doublu, skoro stejne jako deklarace intu (vsechno WIP!)

--- main_int_parser.c
+++ main_int_parser.c
@@ -76,12 +76,51 @@ case INT: // nasleduje deklarovani promenne typu int
            // pokud to nebyl semicolon, jedna se o inicializaci/prirazeni intu 
            
             int_declare_parser(); // volani parseru, ktery overuje syntax inicializace/prirazeni promenne int
-            
-           }
-        }   
-      }      
+        }
+       }
     }
-  }     
+break; // konec case INT
+
+case STRING: // podobne jako u int
+
+    if ((token = lexer(&attr)) == LEX_ERROR) return LEX_ERROR;
+    if (token != ID) return SYNTAX_ERROR; else
+    {
+       tableInsert(blablabla); // vloz identifikator do tabulky symbolu a podivej se, jestli uz takovy v ramci jedne tridy/fce neexistuje, DOPLNIT!!!!!
+
+       if ((token = lexer(&attr)) == LEX_ERROR) return LEX_ERROR;
+       if (token != SEMICOLON && token != EQUAL) return SYNTAX_ERROR; else
+
+       {
+           if (token == SEMICOLON) return class_body(); else // deklarace stringu uvnitr tridy/fce probehla v poradku, rekurzivne volame fci
+           {
+
+           string_declare_parser(); // jedna se o incializaci retezce pri deklaraci, volej parsovaci funkci
+           }
+       }
+    }
+break; // konec case STRING
+
+case DOUBLE:
+
+    if ((token = lexer(&attr)) == LEX_ERROR) return LEX_ERROR;
+    if (token != ID) return SYNTAX_ERROR; else
+    {
+       tableInsert(blablabla); // vloz identifikator do tabulky symbolu a podivej se, jestli uz takovy v ramci jedne tridy/fce neexistuje, DOPLNIT!!!!!
+
+       if ((token = lexer(&attr)) == LEX_ERROR) return LEX_ERROR;
+       if (token != SEMICOLON && token != EQUAL) return SYNTAX_ERROR; else
+
+       {
+           if (token == SEMICOLON) return class_body(); else // deklarace stringu uvnitr tridy/fce probehla v poradku, rekurzivne volame fci
+           {
+
+           double_declare_parser(); // jedna se o incializaci retezce pri deklaraci, volej parsovaci funkci
+           }
+
+       }
+    }
+break; // konec case DOUBLE
            
 
 // PARSER PRO INICIALIZACI CELOCISELNE PROMENNE (WIP)
@@ -153,7 +192,129 @@ int int_declare_parser(){ // parser ktery kontroluje spravny syntax inicializova
 
      // DORESIT CO S ARGUMENTAMA VE FCI
     }
-
 }
+          
+// PARSER PRO INICIALIZACI RETEZCOVE PROMENNE (WIP) skoro stejny jako int          
+          
+ int string_declare_parser(){
+
+    int sign_count = 0; // countery
+    int id_count = 0;
+    int literal_count = 0;
+    int l_bracket_count = 0;
+    int r_bracket_count = 0;
+
+    switch (token){
+
+    case SEMICOLON:
+
+     if ( (id_count == 0 && literal_count == 0) || sign_count == 1) return SYNTAX_ERROR;
+
+     else if ( (id_count == 1 || literal_count == 1 ) && sign_count == 0) return SYNTAX_OK; break; // inicializace intu probehla v poradku
+
+    case STRING_LITERAL:
+
+     literal_count = 1; return string_declare_parser(); break;
+
+    case ID:
+
+     id_count = 1;
+     sign_count = 0;
+
+     // NUTNO PODIVAT SE DO TABULKY SYMBOLU A NAJIT TAM DANY IDENTIFIKATOR POKUD SE JEDNA O ID STRINGU, MUZE SE JEDNAT I O ID FUNKCE KTERA SE NACHAZI ZA PROGRAMEM! DOPLNIT!!!
+
+     tData polozka = tableSearch(blablabla); // podivej se do tabulky na ID, DOPLNIT!!!
+
+     if (polozka.varType == STRING_LITERAL) return string_declare_parser(); break; // pokud odpovida typ ID, rekurzivne parsuj dale, DOPLNIT!!!!!
+
+     else if (polozka.varType != STRING_LITERAL && sign_count == 1 ) KONVERTUJ NA RETEZEC, DOPLNIT!!!!! return string_declare_parser(); break;
+
+     else return SYNTAX_ERROR; break; // pokud to neni retezcovy literal, jedna se o syntakticky error
+
+    case PLUS: // funguje jako konkatenace retezcu
+
+     sign_count = 1; // signalizuj znamenko
+
+     if (id_count == 0 && literal_count == 0) return SYNTAX_ERROR; break; // pokud se nenachazi pred znamenkem id nebo retezcovy literal, je to error
+
+     else return string_declare_parser(); break; // jinak parsuj dale
+
+    default: return SYNTAX_ERROR; break;
+ }
+}
+
+// PARSER PRO INICIALIZACI DESETINNE PROMENNE (WIP) skoro stejny jako int           
+          
+int double_declare_parser(){ // parser ktery kontroluje spravny syntax inicializovania prirazovani hodnot desetinne promenne
+
+    int sign_count = 0; // countery
+    int id_count = 0;
+    int literal_count = 0;
+    int l_bracket_count = 0;
+    int r_bracket_count = 0;
+
+    switch (token){
+
+    case SEMICOLON:
+
+     if ( (id_count == 0 && literal_count == 0) || sign_count == 1) return SYNTAX_ERROR;
+
+     else if ( (id_count == 1 || literal_count == 1 ) && sign_count == 0) return SYNTAX_OK; break;
+
+    case DOUBLE_LITERAL:
+
+     literal_count = 1; return double_declare_parser(); break; // nasledovalo cele cislo, rekurzivne volej fci dale a parsuj
+
+    case ID:
+
+     id_count = 1;
+     literal_count = 0;
+     l_bracket_count = 0;
+     r_bracket_count = 0;
+     sign_count = 0;
+
+     // NUTNO PODIVAT SE DO TABULKY SYMBOLU A NAJIT TAM DANY IDENTIFIKATOR POKUD SE JEDNA O ID INTU, MUZE SE JEDNAT I O ID FUNKCE KTERA SE NACHAZI ZA PROGRAMEM! DOPLNIT!!!
+
+     tData polozka = tableSearch(blablabla); // podivej se do tabulky na ID, DOPLNIT!!!
+
+     if (polozka.varType == DOUBLE_LITERAL) return double_declare_parser(); break; // pokud odpovida typ ID, rekurzivne parsuj dale, DOPLNIT!!!!!
+
+     else return SYNTAX_ERROR; break; // pokud to neni celociselny literal, jedna se o syntakticky error
+
+    case PLUS:
+    case MINUS:
+    case MUL:
+    case DEC:
+
+     sign_count = 1; // signalizuj znamenko
+
+     if (id_count == 0 && literal_count == 0) return SYNTAX_ERROR; break; // pokud se nenachazi pred znamenkem id nebo celociselny literal, je to error
+
+     id_count = 0;
+     literal_count == 0;
+
+     else return double_declare_parser(); break; // jinak parsuj dale
+
+    case LEFT_BRACKET:
+
+     l_bracket_count = 1;
+
+     if (id_count == 1 && literal_count == 0 && sign_count == 0) blablabla // jedna se o funkci, zavolej parser parametru a konce funkci, DOPLNIT!!!
+
+     else if (id_count == 0 && literal_count == 1 && sign_count == 0) return SYNTAX_ERROR; break; // jedna se o syntaktickou chybu typu " int cislo = 5( "
+
+     else if (id_count == 0 && literal_count == 0 && sign_count == 1) // mozna platna deklarace uvidime, DOPLNIT!!!
+
+    case RIGHT_BRACKET:
+
+     r_bracket_count = 1;
+
+     if (l_bracket_count == 1) l_bracket_count == 0; return double_declare_parser(); break;
+
+     else return SYNTAX_ERROR; break;
+
+    default: return SYNTAX_ERROR; break;
+
+    }         
            
            
