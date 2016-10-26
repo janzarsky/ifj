b612b1bb26a63f6dbe223367ac08b3b3d4a74628
davidV23 <vlcedav@seznam.cz>
2016-10-26 19:21:16 +0200

    Create string.h

---
+++ string.h
@@ -0,0 +1,22 @@
+//HLAVICKOVY SOUBOR PRO RETEZCOVOU KNIHOVNU
+
+// prevzaty z ukazkoveho projektu "Zjednodušená implementace interpretu jednoduchého jazyka", zatim beze zmeny
+
+typedef struct
+{
+  char* str;		// misto pro dany retezec ukonceny znakem '\0'
+  int length;		// skutecna delka retezce
+  int allocSize;	// velikost alokovane pameti
+} string;
+
+int strInit(string *s);
+void strFree(string *s);
+
+void strClear(string *s);
+int strAddChar(string *s1, char c);
+int strCopyString(string *s1, string *s2);
+int strCmpString(string *s1, string *s2);
+int strCmpConstStr(string *s1, char *s2);
+
+char *strGetStr(string *s);
+int strGetLength(string *s);
