5ae57e774be8332f0244c1982ce121998161d2a8
LuckaPelantova <pelucik@seznam.cz>
2016-11-01 14:33:45 +0100

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -4,9 +4,16 @@
 #define T_INT    1
 #define T_DOUBLE 2
 #define T_STRING 3
+#include <stdbool.h>
 
 #define TABLE_SIZE 1000
 
+typedef struct string_struct
+{
+  char* str;		// misto pro dany retezec ukonceny znakem '\0'
+  int length;		// skutecna delka retezce
+  int allocSize;	// velikost alokovane pameti
+} string;
 
 typedef struct
 {
@@ -23,7 +30,7 @@ union varValue {
 
 typedef struct tableItem
 {
-  string key;                  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
+  string token;                  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
   tData data;                  // data, ktera jsou ke klici pridruzena
   struct tableItem *nextItem;  // ukazatel na dalsi prvek tabulky symbol
   bool free;
@@ -31,7 +38,7 @@ typedef struct tableItem
 
 typedef struct
 {
-  struct tableItem *table_items[];
+  struct tableItem *table_items[TABLE_SIZE];
 } SymbolTable;
 
 */
