173006a03e1634d7b677940e55eec5b15b560ea4
LuckaPelantova <pelucik@seznam.cz>
2016-11-03 11:44:43 +0100

    Update ial.c

--- ial.c
+++ ial.c
@@ -47,6 +47,61 @@ int readInt()
     return i;
 }
 
+double readDouble ()
+{
+    string *instr;
+    int i = 0;
+    char c;
+    bool dot = false;
+    instr = (string*)malloc(sizeof(string));
+    if (instr == NULL)
+        return -1;
+    instr->str = (char*)malloc(sizeof(char)*STR_LEN_INC);
+    if (instr->str == NULL)
+    {
+        free(instr);
+        return -1;
+    }
+    instr->allocSize = 10;
+    instr->length = 0;
+    instr->str = NULL;
+    c = getchar();
+    while ((c > '0' && c < '9') || c == '.')
+    {
+        if (instr->length + 1 > instr->allocSize)
+        {
+        // pamet nestaci, je potreba provest realokaci
+            if ((instr->str = (char*) realloc(instr->str, instr->length + STR_LEN_INC)) == NULL)
+                return -1;
+            instr->allocSize = instr->length + STR_LEN_INC;
+        }
+        if (c == '.')
+        {
+            if (dot == false)
+                dot = true;
+            else
+            {
+                free(instr->str);
+                free(instr);
+                return -1;
+            }
+        }
+        instr->str[i] = c;
+        instr->length++;
+        c = getchar();
+        i++;
+    }
+    if (c != '\n' || c != EOF)
+    {
+        free(instr->str);
+        free(instr);
+        return -1;
+    }
+    i = atof(instr->str);
+    free(instr->str);
+    free(instr);
+    return i;
+}
 
 char* readString()
 {
