3dea5c734e58ac9a220659cadbf8e8c5ff71d5c1
LuckaPelantova <pelucik@seznam.cz>
2016-11-03 11:31:36 +0100

    Create ial.h

---
+++ ial.h
@@ -0,0 +1,18 @@
+/*
+#ifndef IAL_H_INCLUDED
+#define IAL_H_INCLUDED
+
+#define STR_LEN_INC 10
+typedef struct string_struct
+{
+  char* str;		// misto pro dany retezec ukonceny znakem '\0'
+  int length;		// skutecna delka retezce
+  int allocSize;	// velikost alokovane pameti
+} string;
+
+int readInt ();
+
+char* readString();
+
+#endif // IAL_H_INCLUDED
+*/
