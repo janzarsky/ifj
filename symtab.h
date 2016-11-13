e6ddddbb14c88da7fb2167886e17ef4fad398190
davidV23 <vlcedav@seznam.cz>
2016-11-13 22:11:43 +0100

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -1,45 +1,50 @@
 #ifndef _SYMTAB_H
 #define _SYMTAB_H
 /*
-#define T_INT    1
-#define T_DOUBLE 2
-#define T_STRING 3
 #include <stdbool.h>
+#include "string.h"
 
-#define TABLE_SIZE 1000
+typedef enum {
+    ST_ELEMTYPE_VAR,
+    ST_ELEMTYPE_FUN,
+    ST_ELEMTYPE_CLASS
+} st_elemtype_t;
 
-typedef struct string_struct
-{
-  char* str;		// misto pro dany retezec ukonceny znakem '\0'
-  int length;		// skutecna delka retezce
-  int allocSize;	// velikost alokovane pameti
-} string;
+typedef enum {
+    ST_DATATYPE_INT,
+    ST_DATATYPE_DOUBLE,
+    ST_DATATYPE_STRING,
+    ST_DATATYPE_VOID
+} st_datatype_t;
 
-typedef struct
-{
-  int varType;  // typ dane promenne, v nasem pripade vzdy int
-  union varValue; // pro ruzne typy nutnost resit pomoci unie
-} tData;
+typedef union {
+    int ival;
+    double dval;
+    char *strval;
+} st_value_t;
 
+typedef struct {
+    char *id;
+    st_elemtype_t elem_type;
+    st_datatype_t data_type;
+    st_value_t value;
+    struct symtab_elem_t *nextElem;
+    bool free;
+    string token;
+} symtab_elem_t;
 
-union varValue {
-   int i;
-   float f;
-   char *s;
-};
+typedef struct {
+    symtab_elem_t elements[1000];
+    size_t size;
+} symtab_t;
 
-typedef struct tableItem
-{
-  string token;                  // klic, podle ktereho se bude vyhledavat = nazev identifikatoru
-  tData data;                  // data, ktera jsou ke klici pridruzena
-  struct tableItem *nextItem;  // ukazatel na dalsi prvek tabulky symbol
-  bool free;
-} tTableItem;
-
-typedef struct
-{
-  struct tableItem *table_items[TABLE_SIZE];
-} SymbolTable;
+unsigned int hash_function(const char *str, unsigned htab_size);
+void symbol_init(symtab_t *I, int idx);
+void tableInit(symtab_t *T);
+void symbol_add(string token, symtab_t *tabulka);
+symtab_elem_t *symbol_find (string token, symtab_t *tabulka);
+void symbol_actualize (symtab_t *T, string ident);
+void tableFree(symtab_t *T);
 
 */
 typedef enum {
