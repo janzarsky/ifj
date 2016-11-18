d621565ec31ffb934ab832fb9d92b38cce75a417
LuckaPelantova <pelucik@seznam.cz>
2016-11-18 20:51:09 +0100

    Update symtab.h

--- symtab.h
+++ symtab.h
@@ -1,28 +1,27 @@
 #ifndef _SYMTAB_H
 #define _SYMTAB_H
 /*
+#ifndef _SYMTAB_H
+#define _SYMTAB_H
+
 #include <stdbool.h>
 #include "string.h"
-
 typedef enum {
     ST_ELEMTYPE_VAR,
     ST_ELEMTYPE_FUN,
     ST_ELEMTYPE_CLASS
 } st_elemtype_t;
-
 typedef enum {
     ST_DATATYPE_INT,
     ST_DATATYPE_DOUBLE,
     ST_DATATYPE_STRING,
     ST_DATATYPE_VOID
 } st_datatype_t;
-
 typedef union {
     int ival;
     double dval;
     char *strval;
 } st_value_t;
-
 typedef struct symtab_elem_t{
     char *id;
     st_elemtype_t elem_type;
@@ -32,19 +31,18 @@ typedef struct symtab_elem_t{
     bool free;
     string token;
 } symtab_elem_t;
-
 typedef struct {
     symtab_elem_t elements[1000];
     size_t size;
 } symtab_t;
-
 unsigned int hash_function(const char *str, unsigned htab_size);
-void symbol_init(symtab_t *I, int idx);
-int tableInit(symtab_t *T);
-void symbol_add(string token, symtab_t *tabulka);
-symtab_elem_t *symbol_find (string token, symtab_t *tabulka);
+void symbol_init(symtab_t **I, int idx);
+int tableInit(symtab_t **T);
+void symbol_add(string *token, symtab_t **tabulka);
+symtab_elem_t *symbol_find (string *token, symtab_t *tabulka);
 void symbol_actualize (symtab_t *T, string ident);
 void tableFree(symtab_t *T);
+#endif
 
 */
 typedef enum {
