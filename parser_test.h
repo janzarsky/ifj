d88d4862d933018d86646708ce657fa54092d786
D-k <poplavski.andrei@gmail.com>
2016-11-23 15:09:18 +0100

    q

--- parser_test.h
+++ parser_test.h
@@ -5,6 +5,11 @@
 #define SYNTAX_OK 100
 #define SYNTAX_ERROR 99
 
+//defines from "string.c"
+#define STR_ERROR  -1
+#define STR_SUCCESS 0
+
 extern int token;
+extern void return_token();
 
 #endif
