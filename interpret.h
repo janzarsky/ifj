3f5ab598daf5e355ea945f427dcbb2f7560b7e88
davidV23 <vlcedav@seznam.cz>
2016-11-26 14:39:38 +0100

    Add files via upload

---
+++ interpret.h
@@ -0,0 +1,26 @@
+#ifndef INTERPRET_H_INCLUDED
+#define INTERPRET_H_INCLUDED
+
+typedef union {
+    st_value_t union_value;
+    void *vval;
+} inter_value;
+
+typedef struct inter_stack_item {
+    inter_value value;
+    struct inter_stack_item *next;
+}inter_stack_item;
+
+typedef struct inter_stack{
+    inter_stack_item *top;
+   } inter_stack;
+
+void stack_inter_Pop(inter_stack *S);
+
+void push_tab(st_value_t val, inter_stack *S);
+
+void push_val(void *val, inter_stack *S);
+
+void stack_inter_Top(inter_value *val, inter_stack *S);
+
+#endif // INTERPRET_H_INCLUDED
