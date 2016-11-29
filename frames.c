68e91c21b8d398e546477502e176e305391543f0
Jan Zarsky <xzarsk03@stud.fit.vutbr.cz>
2016-11-29 10:56:20 +0100

    implement fr_free()

--- frames.c
+++ frames.c
@@ -7,16 +7,28 @@
 #include "error_codes.h"
 
 frame_t *new_frame = NULL;
-frame_t *old_frame = NULL;
 frame_t *active_frame = NULL;
 
 void fr_print(frame_t *frame) {
-    printf("frame: \n");
-
     frame_item_t *temp = frame->first_item;
 
     while (temp != NULL) {
         printf("frame_item: var: %p, value: %i\n", (void *)temp->var, temp->value.ival);
+        temp = temp->next;
+    }
+}
+
+void fr_print_frames() {
+    if (new_frame != NULL) {
+        printf("INTERPRET: new_frame: \n");
+        fr_print(new_frame);
+    }
+
+    frame_t *temp = active_frame;
+
+    while (temp != NULL) {
+        printf("INTERPRET: active_frame: \n");
+        fr_print(temp);
         temp = temp->next;
     }
 }
@@ -94,6 +106,19 @@ int fr_get(frame_t *frame, symtab_elem_t *var, st_value_t *val) {
     return 0;
 }
 
+void fr_free(frame_t **frame) {
+    frame_item_t *item = (*frame)->first_item;
+
+    while (item != NULL) {
+        frame_item_t *temp = item;
+        item = item->next;
+        free(temp);
+    }
+
+    free(*frame);
+    *frame = NULL;
+}
+
 int call_instr(tListOfInstr *instrlist, inter_stack *stack, symtab_elem_t *func) {
     printf("INTERPRET: inside call instruction, params: %p %p %p\n", (void *) instrlist, (void *) stack, (void *) func);
 
@@ -110,8 +135,7 @@ int call_instr(tListOfInstr *instrlist, inter_stack *stack, symtab_elem_t *func)
         param = param->next_param;
     }
 
-    printf("INTERPRET: ");
-    fr_print(new_frame);
+    fr_print_frames();
 
     new_frame->next = active_frame;
     active_frame = new_frame;
@@ -126,14 +150,14 @@ int return_instr(symtab_t *symtab, tListOfInstr *instrlist) {
     if (active_frame == NULL)
         return INTERNAL_ERROR;
 
-    printf("INTERPRET: ");
-    fr_print(active_frame);
+    fr_print_frames();
 
     listGoto(instrlist, active_frame->next_instr);
 
-    // TODO free frame
-
+    frame_t *temp = active_frame;
     active_frame = active_frame->next;
+
+    fr_free(&temp);
 
     return ER_OK;
 }
@@ -142,11 +166,9 @@ st_value_t get_value(symtab_elem_t *var) {
     st_value_t val;
     fr_get(active_frame, var, &val);
     return val;
-
-    //return var->value;
 }
 
 void set_value(symtab_elem_t *var, inter_value *value) {
     fr_set(active_frame, var, value->union_value);
-    //var->value = value->union_value;
+    fr_print_frames();
 }
