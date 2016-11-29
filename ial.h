65d2c6169cc19820fb33a33b3f876a1fc8f7aa4f
vitmrlik <vit.mrlik@gmail.com>
2016-11-29 15:47:38 +0100

    odebrany deklarace 'read...' funkci

--- ial.h
+++ ial.h
@@ -2,17 +2,8 @@
 #ifndef IAL_H_INCLUDED
 #define IAL_H_INCLUDED
 
-#define STR_LEN_INC 10
-typedef struct string_struct
-{
-  char* str;		// misto pro dany retezec ukonceny znakem '\0'
-  int length;		// skutecna delka retezce
-  int allocSize;	// velikost alokovane pameti
-} string;
-
-int readInt ();
-double readDouble ();
-char* readString();
+//deklaraci teto konstanty bych radsi nechal souboru string ..?
+//#define STR_LEN_INC 8
 
 char *sort(char *s);
 int find(char *s, char *search);
