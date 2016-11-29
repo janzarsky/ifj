288ccc57d52543e841b1e405e00f348cc4aab0eb
vitmrlik <vit.mrlik@gmail.com>
2016-11-29 15:03:25 +0100

    odebrany funkce read
    duvod: nejsem si 100pro jisty, ale v tomto souboru bych nechal jen to, co je napsano v zadani (kapitola 13.2)

--- ial.c
+++ ial.c
@@ -4,145 +4,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int readInt()
-{
-    string *instr;
-    int i = 0;
-    char c;
-    instr = (string*)malloc(sizeof(string));
-    if (instr == NULL)
-        return -1;
-    instr->str = (char*)malloc(sizeof(char)*STR_LEN_INC);
-    if (instr->str == NULL)
-    {
-        free(instr);
-        return -1;
-    }
-    instr->allocSize = 10;
-    instr->length = 0;
-    instr->str = NULL;
-    c = getchar();
-    while (c > '0' || c < '9')
-    {
-        if (instr->length + 1 > instr->allocSize)
-        {
-        // pamet nestaci, je potreba provest realokaci
-            if ((instr->str = (char*) realloc(instr->str, instr->length + STR_LEN_INC)) == NULL)
-                return -1;
-            instr->allocSize = instr->length + STR_LEN_INC;
-        }
-        instr->str[i] = c;
-        instr->length++;
-        c = getchar();
-        i++;
-    }
-    if (c != '\n' || c != EOF)
-    {
-        free(instr->str);
-        free(instr);
-        return -1;
-    }
-    i = atoi(instr->str);
-    free(instr->str);
-    free(instr);
-    return i;
-}
-
-double readDouble ()
-{
-    string *instr;
-    int i = 0;
-    char c;
-    bool dot = false;
-    instr = (string*)malloc(sizeof(string));
-    if (instr == NULL)
-        return -1;
-    instr->str = (char*)malloc(sizeof(char)*STR_LEN_INC);
-    if (instr->str == NULL)
-    {
-        free(instr);
-        return -1;
-    }
-    instr->allocSize = 10;
-    instr->length = 0;
-    instr->str = NULL;
-    c = getchar();
-    while ((c > '0' && c < '9') || c == '.')
-    {
-        if (instr->length + 1 > instr->allocSize)
-        {
-        // pamet nestaci, je potreba provest realokaci
-            if ((instr->str = (char*) realloc(instr->str, instr->length + STR_LEN_INC)) == NULL)
-                return -1;
-            instr->allocSize = instr->length + STR_LEN_INC;
-        }
-        if (c == '.')
-        {
-            if (dot == false)
-                dot = true;
-            else
-            {
-                free(instr->str);
-                free(instr);
-                return -1;
-            }
-        }
-        instr->str[i] = c;
-        instr->length++;
-        c = getchar();
-        i++;
-    }
-    if (c != '\n' || c != EOF)
-    {
-        free(instr->str);
-        free(instr);
-        return -1;
-    }
-    i = atof(instr->str);
-    free(instr->str);
-    free(instr);
-    return i;
-}
-
-char* readString()
-{
-    string *instr;
-    int i = 0;
-    char c;
-    char *b;
-    instr = (string*)malloc(sizeof(string));
-    if (instr == NULL)
-        return -1;
-    instr->str = (char*)malloc(sizeof(char)*STR_LEN_INC);
-    if (instr->str == NULL)
-    {
-        free(instr);
-        return -1;
-    }
-    instr->allocSize = 10;
-    instr->length = 0;
-    instr->str = NULL;
-    c = getchar();
-    while (c != '\n' || c != EOF)
-    {
-        if (instr->length + 1 > instr->allocSize)
-        {
-        // pamet nestaci, je potreba provest realokaci
-            if ((instr->str = (char*) realloc(instr->str, instr->length + STR_LEN_INC)) == NULL)
-                return -1;
-            instr->allocSize = instr->length + STR_LEN_INC;
-        }
-        instr->str[i] = c;
-        instr->length++;
-        c = getchar();
-        i++;
-    }
-    free(instr->str);
-    free(instr);
-    //return string?
-    return ;
-}
-
 //pomocna funkce sortu
 void SiftDown(char *A, int Left, int Right)
 {
@@ -165,14 +26,13 @@ void SiftDown(char *A, int Left, int Right)
   A[i] = Temp;  //konecne umisteni proseteho uzlu
 }
 
-//Seradi znaky v danem retezci 's' tak, aby znak s nizsi ordinalni hodnotou vzdy predchazel znaku s vyssi
-// ordinalni hodnotou. Algoritmus: HEAP-SORT
 char *sort(char *s)
 {
   //vytvoreni noveho stringu, ktery bude obsahovat serazene znaky
   char *sor;
-  if ((sor = (char*) malloc ( (strlen(s))+2 ) )== NULL)
-    printf("CHYBA pri alokaci\n"); //error
+  int need_size = ((strlen(s) + 1) / STR_LEN_INC) + 1;  //pomocna promenna, aby byl alokovany prostor nasobek osmi
+  if ((sor = (char*) malloc(sizeof(char) * STR_LEN_INC * need_size)) == NULL)
+    printf("CHYBA pri alokaci\n"); //error, num. 99
 
   strcpy(sor, s);
 
