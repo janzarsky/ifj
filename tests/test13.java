e47ccd699bb81db48a6a17567d7a202c2b61938e
LuckaPelantova <pelucik@seznam.cz>
2016-12-06 11:00:49 +0100

    Add files via upload

---
+++ tests/test13.java
@@ -0,0 +1,33 @@
+// exit 0
+// output 1
+class Main {
+    static void run() {
+        int a;
+        double b;
+        a = b + 5;
+        blight.darspawn();
+    }
+}
+
+class blight {
+    static void darkspawn (){
+        int hurlock;
+        double shriek;
+        double gemlock;
+        archdemon.dragonling();
+    }
+}
+
+class archdemon {
+    static void dragonling () {
+        int dragon_eggs = 5;
+        grey_wardens.slay();
+    }
+}
+
+class grey_wardens {
+    static void slay () {
+        int hero = 1;
+        ifj16.print(hero + "\n");
+    }
+}
